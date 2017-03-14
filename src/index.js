64320e8bdafbe26e4ff90a678e2e731d4363f77a
Tony Polinelli <tonypolinelli@gmail.com>
2017-03-14 12:31:06 -0700

    dont import global styles

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import 'google-closure-library/closure/goog/demos/css/demo.css'
-import 'google-closure-library/closure/goog/css/dialog.css'
-import 'google-closure-library/closure/goog/css/button.css'
-import 'google-closure-library/closure/goog/css/dialog.css'
-import 'google-closure-library/closure/goog/css/linkbutton.css'
-import 'google-closure-library/closure/goog/css/menu.css'
-import 'google-closure-library/closure/goog/css/menuitem.css'
-import 'google-closure-library/closure/goog/css/menuseparator.css'
-import 'google-closure-library/closure/goog/css/tab.css'
-import 'google-closure-library/closure/goog/css/tabbar.css'
-import 'google-closure-library/closure/goog/css/toolbar.css'
-import 'google-closure-library/closure/goog/css/colormenubutton.css'
-import 'google-closure-library/closure/goog/css/palette.css'
-import 'google-closure-library/closure/goog/css/colorpalette.css'
-import 'google-closure-library/closure/goog/css/editor/bubble.css'
-import 'google-closure-library/closure/goog/css/editor/dialog.css'
-import 'google-closure-library/closure/goog/css/editor/linkdialog.css'
-import 'google-closure-library/closure/goog/css/editortoolbar.css'
+import 'google-closure-library/closure/goog/css/dialog.css';
+import 'google-closure-library/closure/goog/css/button.css';
+import 'google-closure-library/closure/goog/css/dialog.css';
+import 'google-closure-library/closure/goog/css/linkbutton.css';
+import 'google-closure-library/closure/goog/css/menu.css';
+import 'google-closure-library/closure/goog/css/menuitem.css';
+import 'google-closure-library/closure/goog/css/menuseparator.css';
+import 'google-closure-library/closure/goog/css/tab.css';
+import 'google-closure-library/closure/goog/css/tabbar.css';
+import 'google-closure-library/closure/goog/css/toolbar.css';
+import 'google-closure-library/closure/goog/css/colormenubutton.css';
+import 'google-closure-library/closure/goog/css/palette.css';
+import 'google-closure-library/closure/goog/css/colorpalette.css';
+import 'google-closure-library/closure/goog/css/editor/bubble.css';
+import 'google-closure-library/closure/goog/css/editor/dialog.css';
+import 'google-closure-library/closure/goog/css/editor/linkdialog.css';
+import 'google-closure-library/closure/goog/css/editortoolbar.css';
 
-import 'imports-loader?this=>window!../dist/editor.compiled.js' // loads the closure library at window.growlabs
+import 'imports-loader?this=>window!../dist/editor.compiled.js'; // loads the closure library at window.growlabs
 
-export default window.growlabs.ClosureEditor
+export default window.growlabs.ClosureEditor;
